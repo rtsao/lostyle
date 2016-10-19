3635aa6f1294dfa6957eea5ac81711e9a38f28d6
Ryan Tsao <rtsao@uber.com>
2016-10-18 22:00:03 -0700

    Improved border JSDoc

--- src/border.js
+++ src/border.js
@@ -8,11 +8,32 @@ import borderRadius from './borderRadius.js';
  * Shorthand border helper
  * @function border
  * @param  {object}  value
- * @param  {object} [value.color]  border color value
- * @param  {object} [value.style]  border style value
- * @param  {object} [value.width]  border with value
- * @param  {object} [value.radius] border radius value
- * @return {object}                Style object
+ * @param  {number|string|object} [value.color]  border color value (see {@link borderColor})
+ * @param  {number|string|object} [value.style]  border style value (see {@link borderStyle})
+ * @param  {number|string|object} [value.width]  border width value (see {@link borderWidth})
+ * @param  {number|string|object} [value.radius] border radius value (see {@link borderRadius})
+ * @return {object}                              Style object
+ * @example
+ * // A single property in all directions
+ * border({color: 'red'});
+ * // → {
+ * //     borderTopColor: 'red',
+ * //     borderRightColor: 'red',
+ * //     borderBottomColor: 'red',
+ * //     borderLeftColor: 'red'
+ * //   }
+ * @example
+ * // Any combination
+ * border({color: 'red', width: {y: '2px', right: '8px'}});
+ * // → {
+ * //     borderTopColor: 'red',
+ * //     borderRightColor: 'red',
+ * //     borderBottomColor: 'red',
+ * //     borderLeftColor: 'red',
+ * //     borderTopWidth: '2px',
+ * //     borderRightWidth: '8px',
+ * //     borderBottomWidth: '2px'
+ * //   }
  */
 export default function border(arg) {
   return assign({},
